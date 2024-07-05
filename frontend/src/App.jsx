5cccf79c7b3a6fab7e4defa8ed83c59112d68219
ShehzadKhuwaja <shehzadkhuwaja@gmail.com>
2024-07-06 01:24:07 +0500

    basic layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Box, Container } from '@mui/material'
+import Stack from '@mui/material/Stack'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container sx={{ backgroundColor: 'green', width: '80vw', height: '100vh'}}>
+      <Stack direction='row' spacing={0} sx={{ backgroundColor: 'lightblue', height: '100%'}}>
+        <Box sx={{ backgroundColor: 'red', width: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          login
+        </Box>
+        <Box sx={{ backgroundColor: 'yellow', p: 2, width: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          Sign Up
+        </Box>
+      </Stack>
+    </Container>
   )
 }
 
