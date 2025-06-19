c6c91364eb3e1d44ffe408d305a6995ad9cbcee0
ShehzadKhuwaja <shehzadkhuwaja@gmail.com>
2025-06-19 12:23:43 +0500

    containerizing app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
         '/api': {
-            target: 'http://localhost:3000',
+            target: 'http://backend_service:5000',
             changeOrigin: true
         }
     }
